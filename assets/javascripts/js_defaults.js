cee87c779bd3639cf7f3b6107008e81ff5340cd0
Jan Molic <jan.molic@1984.cz>
2016-11-21 12:15:03 +0100

    All changes in the master are back to the production state. They will be moved to the taras branch.

--- assets/javascripts/js_defaults.js
+++ assets/javascripts/js_defaults.js
@@ -2,6 +2,5 @@
 //= require jquery_ujs
 //= require jquery-ui/effect
 //= require jquery-ui/sortable
-//= require bootstrap.min
 //= require json2
 //= require application
--- assets/javascripts/js_index.js
+++ assets/javascripts/js_index.js
@@ -2,4 +2,3 @@
 //= require wz_dragdrop
 //= require popups
 //= require addthis_welcome_init
-//= require back_to_top
