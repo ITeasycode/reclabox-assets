fab1dffc4e2eff8d0b25457d800d7fb18698fb15
White <vtonkopey@gmail.com>
2017-06-07 15:32:18 +0300

    new update

--- assets/template/src/js/components/tooltips.js
+++ assets/template/src/js/components/tooltips.js
@@ -2,18 +2,24 @@
 // -----------------------------------------
 ;( function ( $ ) {
     $( function () {
-        var $links = $( '.links > .btn-floating' );
+        var $links = $( '.links > .btn-floating, .help > .btn-floating' );
 
         $links.on( 'click', function ( event ) {
             removeEventDefault( event );
 
             var $funcTooltips = $( this ).next( '.functional-tooltips' );
             var thisPosition = $( this ).position().left;
+            console.log('position tooltips', thisPosition);
 
             if ( $funcTooltips.hasClass( 'hide' ) ) {
-                $funcTooltips
-                    .removeClass( 'hide' )
-                    .css( 'left', -( thisPosition + 2 ) );
+                if ( thisPosition !== 0 ) {
+                    $funcTooltips
+                        .removeClass( 'hide' )
+                        .css( 'left', -( thisPosition + 2 ) );
+                } else {
+                    $funcTooltips
+                        .removeClass( 'hide' );
+                }
             } else {
                 $funcTooltips.addClass( 'hide' );
             }
