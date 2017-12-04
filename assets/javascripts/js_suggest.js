c87fe7b245424bfd313818f3e411dfaa17dc085e
kinza1 <kinza1@mail.ru>
2017-12-04 15:23:45 +0200

    Fixes December 4

--- assets/javascripts/js_suggest.js
+++ assets/javascripts/js_suggest.js
@@ -1,2 +1,12 @@
+//= require jquery
 //= require suggest
 //= require suggest_custom
+$( function() {
+    $( "#search-input" ).autocomplete({
+        source: '/firms/autocomplete.json',
+        select: function(event, ui) {
+            $("#search-input").val(ui.item.value);
+            $("#search-input").parent().parent().submit();
+        }
+    });
+} );
