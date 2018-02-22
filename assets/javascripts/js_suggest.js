039a0efb15d229bec321bc37b5f7317f90259806
Sasha Sydorenko <sashokua91@gmail.com>
2018-02-22 13:36:33 +0200

    remove unnecessary files

--- assets/javascripts/js_suggest.js
+++ assets/javascripts/js_suggest.js
@@ -2,11 +2,11 @@
 //= require suggest
 //= require suggest_custom
 $( function() {
-    $( "#search-input" ).autocomplete({
-        source: '/firms/autocomplete.json',
-        select: function(event, ui) {
-            $("#search-input").val(ui.item.value);
-            $("#search-input").parent().parent().submit();
-        }
-    });
+    // $( "#search-input" ).autocomplete({
+    //     source: '/firms/autocomplete.json',
+    //     select: function(event, ui) {
+    //         $("#search-input").val(ui.item.value);
+    //         $("#search-input").parent().parent().submit();
+    //     }
+    // });
 } );
